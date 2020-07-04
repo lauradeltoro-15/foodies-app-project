b5d3e5201d7053349c586acf238d5a89ab9ed4ed
Laura del Toro <lauradeltoro.arq@gmail.com>
2020-07-04 16:08:07 +0200

    Search recipes with filters

--- routes/recipes/index.js
+++ routes/recipes/index.js
@@ -3,30 +3,36 @@ const router = express.Router()
 const recipeApi = require("../../api-handler/recipe-handler")
 
 //Helpers
-const renderAllRecipeInformationsByIds = (ids,res) => {
+const renderAllRecipeInformationsByIds = (ids, res, req) => {
     Promise.all(ids.map(id => recipeApi.getRecipeInformationById(id)
-            .then(response => response)))
-            .then(response => {
-                console.log(response)
-                res.render("recipes/search-recipes", {results: response})
-            })
-            .catch(err => console.log("There was an error returning from ddbb", err))
+            .then(response => response)
+            .catch(err => console.log("There was an error returning from DDBB", err))))
+        .then(recipes => req.query.filter ? filterRecipes(recipes, req) : recipes)
+        .then(response => res.render("recipes/search-recipes", {
+            results: response
+        }))
+        .catch(err => console.log("There was an error returning from ddbb", err))
+}
+
+const filterRecipes = (recipes, req) => {
+    const filters = [...req.query.filter]
+    return recipes.filter(recipe => filters.every(filter => recipe[filter]))
 }
 
 //Routes
 router.get('/details/:recipeID', (req, res) => {
     recipeApi.getRecipeInformationById(req.params.recipeID)
-    .then(detailedRecipe => res.render("recipes/detailed-recipe", detailedRecipe))
+        .then(detailedRecipe => res.render("recipes/detailed-recipe", detailedRecipe))
 })
 
 router.post('/add-to-favourites/:recipeID', (req, res) => res.send('añadido a favoritos'))
 router.get('/search', (req, res) => {
     recipeApi.getFullList(req.query.query)
-        .then(response => response.results.map(result =>result.id))
-        .then(ids => renderAllRecipeInformationsByIds(ids, res))
+        .then(response => response.results.map(result => result.id))
+        .then(ids => renderAllRecipeInformationsByIds(ids, res, req))
         .catch(err => console.log("There was an error returning from ddbb", err))
 
-    })
+})
 
 router.get('/', (req, res) => res.render("recipes/search-recipes"))
 
@@ -36,7 +42,12 @@ module.exports = router
 
 
 
-// const ids = response.results.map(result =>result.id)
-// ids.forEach(id => recipeApi.getRecipeInformationById(id)
-// .then(response => console.log(response)))
-// res.render("recipes/search-recipes", {response})
+// const renderAllRecipeInformationsByIds = (ids,res) => {
+//     Promise.all(ids.map(id => recipeApi.getRecipeInformationById(id)
+//             .then(response => response)))
+//             .then(response => {
+//                 console.log("Here the response" , response)
+//                 res.render("recipes/search-recipes", {results: response})
+//             })
+//             .catch(err => console.log("There was an error returning from ddbb", err))
+// }
