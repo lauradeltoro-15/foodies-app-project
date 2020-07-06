251853d3b5e2e601da03526a1333153becf0fe67
Laura del Toro <lauradeltoro.arq@gmail.com>
2020-07-06 21:15:52 +0200

    Addition to weekplanner

--- routes/profile/my-week.routes.js
+++ routes/profile/my-week.routes.js
@@ -9,21 +9,39 @@ const Weekmeal = require("../../models/week-meal.model")
 const isLoggedIn = (req, res, next) => req.isAuthenticated() ? next() : res.render("auth/login", {
     errorMsg: "Restricted area!"
 })
+
+const getDayOffset = (dateToCompare) => {
+    const today = new Date()
+    return dateToCompare - today.getDate()
+
+}
+
 // Endpoints
 router.post("/delete/:recipeId", (req, res) => res.send("Deleting"))
 
 
-router.get("/:userId",isLoggedIn, (req, res) => {
+router.get("/:userId", isLoggedIn, (req, res) => {
     Weekmeal.find()
-    .populate("Weekmeal")
-    .then()
-    console.log(req.user)
+        .populate("Weekmeal")
+        .then()
     res.render(`profile/my-week`, {
         user: req.user
     })
 })
-router.get('/',isLoggedIn, (req, res) => {
-    res.redirect(`/profile/my-week/${req.user.id}`)
+router.get('/', isLoggedIn, (req, res) => {
+    Weekmeal.find({
+            owner: req.user.id
+        })
+        .then(weekmeals => {
+            return {
+                day1: weekmeals.filter(meal => getDayOffset(meal.mealDay) === 0)
+            }
+        }).then(day => {
+            console.log(day)
+            res.render("profile/my-week")
+        })
+        .catch(err => console.log("There was an error returning from DDBB", err))
+
 })
 
 module.exports = router
