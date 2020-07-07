6bac50016212938b7bbecd9c02b5fb1f8ca16c54
Laura del Toro <lauradeltoro.arq@gmail.com>
2020-07-07 14:31:47 +0200

    Shopping list ingredients rendered

--- routes/profile/my-shopping-list.routes.js
+++ routes/profile/my-shopping-list.routes.js
@@ -4,10 +4,20 @@ const passport = require("passport")
 
 const Recipe = require('../../models/recipe.model')
 const User = require("../../models/user.model")
+const Weekmeal = require("../../models/week-meal.model")
 
 const isLoggedIn = (req, res, next) => req.isAuthenticated() ? next() : res.render("auth/login", {
     errorMsg: "Restricted area!"
 })
+const filterIngredients = weekmeals => filterUniqueIngredients(filterIngredientsByRecipeDay(weekmeals))
+
+
+const filterIngredientsByRecipeDay = weekmeals => {
+    const today = (new Date()).getDate()
+    const lastDay = today + 6
+    return weekmeals.filter(meal => meal.mealDay.getDate() >= today && meal.mealDay.getDate() <= lastDay).map(meal => meal.ingredients).flat();
+}
+const filterUniqueIngredients = (ingredients) => ingredients.filter((ingredient, i) => ingredients.indexOf(ingredient) === i && ingredient !== null)
 
 router.get("/add", (req, res) => res.send("Here I add to the shopping list"))
 router.post("/add", (req, res) => res.send("Here I finish to add to the shopping list"))
@@ -16,10 +26,16 @@ router.get("/edit", (req, res) => res.send("Here I edit to the shopping list"))
 router.post("/edit", (req, res) => res.send("Here I finish to edit to the shopping list"))
 
 router.get("/:userId", isLoggedIn, (req, res) => {
-    console.log(req.user)
-    res.render(`profile/my-shopping-list`, {
-        user: req.user
-    })
+
+    Weekmeal.find({}, {
+            mealDay: 1,
+            ingredients: 1
+        })
+        .then(weekmeals => filterIngredients(weekmeals))
+        .then(filteredWeekMeals => res.render(`profile/my-shopping-list`, {
+            filteredWeekMeals
+        }))
+        .catch(err => console.log("There was an error returning from DDBB", err))
 })
 router.get('/', isLoggedIn, (req, res) => {
     res.redirect(`/profile/my-shopping-list/${req.user.id}`)
